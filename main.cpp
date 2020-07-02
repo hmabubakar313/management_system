43caeebde8176fbfd6db1e8b4120c740b7578419
Aqib-Ahmed <66301109+Aqib-Ahmed@users.noreply.github.com>
2020-07-02 16:34:51 +0500

    geting user data class added

--- main.cpp
+++ main.cpp
@@ -1,165 +1,280 @@
+// Semaster_Project.cpp : Defines the entry point for the console application.
+//
+
+//#include "stdafx.h"
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
-#include <string>
-// #include <cstring>
 #include <cmath>
+//#include <string>
+//#include <conio.h>
+
 using namespace std;
-class Employe
-{
-    public:
-    string first_name,last_name,cust_father_name,cust_mother_name,cust_cnic;
-    int key,customer_id,account_number,card_number,card_id;
-    //*setter and getter start from here
-    void set_cust_id(int ci)
-    {
-        customer_id = ci;
-    }
-    int get_cust_id()
-    {
-        return customer_id;
-    }
-    
-    void set_first_name(string f_n)
-    {
-        first_name = f_n;
-    }
-    string get_first_name()
-    {
-        return first_name;
-    }
-    void set_last_name(string l_n)
-    {
-        last_name = l_n;
-    }
-    string get_last_name()
-    {
-        return last_name;
-    }
-    void set_father_name(string father_n)
-    {
-        cust_father_name = father_n;
-    }
-    string get_father_name()
-    {
-        return cust_father_name;
-    }
-    void set_mother_name(string m_n)
-    {
-        cust_mother_name = m_n;
-    }
-    string get_mother_name()
-    {
-        return cust_mother_name;
-    }
-    void set_cust_cnic(string c_cnic)
-    {
-        cust_cnic = c_cnic;
-    }
-    string get_cust_cnic()
-    {
-        return cust_cnic;
-    }
-    void set_key(int k)
-    {
-        key = k;
-    }
-    int get_key()
-    {
-        return key;
-    }
-    void set_account_number(int a_number)
-    {
-        account_number = a_number;
-    }
-    int get_account_number()
-    {
-        return account_number;
-    }
-    void set_card_number(int c_number)
-    {
-        card_number = c_number;
-    }
-    int get_card_number()
-    {
-        return card_number;
-    }
-    void set_card_id(int c_id)
-    {
-        card_id = c_id;
-    }
-    int get_card_id()
-    {
-        return card_id;
-    }
-    //!setter and getter end here
-    
-    
+
+class user{
+protected:
+	char F_name[10],L_name[10],email[25],mobi[12],type;
+	double age,i,Acc_ID,pass,blnc;
+public:
+	void userdata(){
+		cout<<"\n\t\t\t\t\t Enter First Name : ";
+		cin>>F_name;
+		cout<<"\n\t\t\t\t\t Enter last Name : ";
+		cin>>L_name;
+		cout<<"\n\t\t\t\t\t Enter Email-ID : ";
+		cin>>email;
+		cout<<"\n\t\t\t\t\t Enter Age : ";
+		cin>>age;
+		cout<<"\n\t\t\t\t\t Enter Mobile No : ";
+		cin>>mobi;
+	}
 };
-class Customer :public Employe
-{
-    public:
-        // void get_data()
-        // {
-        //     cout<<"Welcome to the ----- Bank\n";           //!stylling for Aqib
-        //     cout<<"Enter Your First Name\n";
-        //     cin>>first_name;
-        //     cout<<"Enter Your Last Name\n";
-        //     cin>>last_name;
-        //     cout<<"Enter Your father Name\n";
-        //     cin>>cust_father_name;
-        //     cout<<"Enter Your Mother Name\n";
-        //     cin>>cust_mother_name;
-        //     cout<<"Enter Your CNIC\n";
-        //     cin>>cust_cnic;
-        //     cout<<"Press 1 to generate customer ID\n";
-        //     cin>>key;
-        //     // * generating customer id
-        //     if (key==1) 
-        //     {
-        //         srand(time(0));
-        //         customer_id = (rand() % 100) + 1; // use for random number
-        //     }
-        //     cout<<"Congratulation your account created and your Customer ID is : 00"<<customer_id<<endl;
+class Account : public user{
+public:
+	void UserData(){
+		cout<<"\n\t\t\t\t\t Enter Account-ID : ";
+		cin>>Acc_ID;
+		do{
+			cout<<"\n\t\t\t\t\t Enter type Account..(C/S) : ";
+			cin>>type;
+			if(type=='s' || type =='S')
+			{
+				cout<<"\n\t\t\t\t\t Enter Initial Amount (>= Rs.500) : ";
+				cin>>blnc;
+				if(blnc<500){
+					cout<<"\n\t\t\t\t Please Enter greater than Rs.500"<<endl;
+				}
+				i=1;
+			}
+			else if(type=='C' || type =='c')
+			{
+				cout<<"\n\t\t\t\t\t Enter Initial Amount (>= Rs.1000) : ";
+				cin>>blnc;
 
-        // }
-
-        // getting data for cards detail and checkbook
-
-        void get_data_cards()
-        {
-            // cout<<"Welcome to the ----- Bank\n";           //!stylling for Aqib
-            // cout<<"Enter Your First Name\n";
-            // cin>>first_name;
-            // cout<<"Enter Your Last Name\n";
-            // cin>>last_name;
-            // cout<<"Enter Your father Name\n";
-            // cin>>cust_father_name;
-            // cout<<"Enter Your Mother Name\n";
-            // cin>>cust_mother_name;
-            // cout<<"Enter Your CNIC\n";
-            // cin>>cust_cnic;
-            // cout<<"Enter Your Account Number\n";
-            // cin>>account_number;
-            cout<<"Press 1 to generate customer ID\n";
-            cin>>key;
-            // * generating customer id
-            if (key==1) 
-            {
-                srand(time(0));
-                card_number = (rand() % 100000000) + 1; // use for random number
-            }
-            cout<<"Congratulation your account created and your Credit and Debit Card ID is : 00"<<card_id<<endl;
-
-        }
-         
-
+				if(blnc<1000){
+					cout<<"\n\t\t\t\t Please Enter greater than Rs.1000"<<endl;
+				}
+				i=1;
+			}
+			else
+			{
+				cout<<"\n\t\t\t\t Try Again Press 'c' for Current Account , 's' for Saving Account"<<endl;
+				i=0;
+			}
+		}while(i==0);
+		cout<<"\n\t\t\t\t\t Enter Password : ";
+		cin>>pass;
+	}
+	void nam()       // Name display
+	{
+		cout<<F_name<<"  "<<L_name;cout<<"\n+++++++++++++++++++++++++++++++++++++++++++";
+	}
 };
+class Transaction : public Account    // class Transaction  , Account Inherit
+{
+	int amnt,p,t;
+	float r,ci;
+public:
+	Transaction()
+	{
+		amnt=0;
+	}
+	void displ()
+	{
+		cout<<"\n+++++++++++++++++++++++++++++++++++++++++++";
+		cout<<"\n\t Account Name is "<<F_name<<" "<<L_name;
+		cout<<"\n\t Balance is "<<blnc;
+		cout<<"\n+++++++++++++++++++++++++++++++++++++++++++\n";
+		//	getch();
+	}     
+	void deposit()   // Deposit 
+	{
+		cout<<"\n\t  Enter amount to Deposit : ";
+		cin>>amnt;
+		cout<<"\n+++++++++++++++++++++++++++++++++++++++++++\n";
+		if(amnt>=100)
+		{
+			blnc=blnc+amnt;
+		}
+		else
+		{
+			cout<<"\n\t Amount Should be Greater than equal Rs.100 "<<endl<<endl<<endl;
+		}
+		cout<<"\n\t You have Deposited Successfully ";
+		cout<<"\n+++++++++++++++++++++++++++++++++++++++++++";
+		cout<<"\n\t Current Balance is Rs."<<blnc;
+		cout<<"\n+++++++++++++++++++++++++++++++++++++++++++";
+		//	getch(); 
+	}   
+	void withdraw()   // Withdraw
+	{
+		cout<<"\n\t  Enter amount to Withdraw : ";
+		cin>>amnt;
+		cout<<"\n+++++++++++++++++++++++++++++++++++++++++++\n";
+		if(amnt>=100)
+		{
+			if(blnc-amnt<0)
+			{
+				cout<<"\n\t Insufficient Balance ";
+				//				getch();
+			}        
+			else
+				blnc=blnc-amnt;
+		}
+		else
+		{
+			cout<<"\n\tAmount Should be Greater than equal Rs.100 "<<endl<<endl<<endl;
+		}
+		cout<<"\n+++++++++++++++++++++++++++++++++++++++++++";
+		cout<<"\n\t You have Withdrawl Successfully ";
+		cout<<"\n+++++++++++++++++++++++++++++++++++++++++++";
+		if(type=='c' || type=='C')
+		{
+			if(blnc<1000)
+			{
+				cout<<"\n\t Your have cross the limit of minimum balance charges may apply ";
+				blnc=blnc-100;
+			}
+		}
+		cout<<"\n+++++++++++++++++++++++++++++++++++++++++++";
+		cout<<"\n\t Current Balance is Rs."<<blnc;
+		cout<<"\n+++++++++++++++++++++++++++++++++++++++++++";
+		//	getch(); 
+	} 
+	void blncenquiry()     // Balance Enquiry
+	{
+		cout<<"\n\t   Name : "<<F_name<<" "<<L_name;
+		cout<<"\n\t   Account No : "<<Acc_ID;
+		if(type=='s' || type== 'S')
+			cout<<"\n\t   Type of Account : Saving";
+		else
+			cout<<"\n\t   Type of Account : Current"; 
+		cout<<"\n+++++++++++++++++++++++++++++++++++++++++++";
+		cout<<"\n\t   Your Available balance is Rs."<<blnc;
+		cout<<"\n+++++++++++++++++++++++++++++++++++++++++++";
+		//	getch();
+	} 
+	void checkbk()
+	{
+		if(type=='c' || type=='C')
+		{
+			system("cls");
+			cout<<"\n\t Check Book Facility Available";
+			cout<<"\n+++++++++++++++++++++++++++++++++++++++++++";
+			//			getch();
+		}
+		else
+		{
+			//system("cls");
+			cout<<"\n\t Check Book Facility Not Available";
+			cout<<"\n+++++++++++++++++++++++++++++++++++++++++++";
+			//	getch();
+		}
+	} 
+	void compound()
+	{
+		system("cls");
+		if(type=='s' || type=='S')
+		{
+			cout<<"\n\t   Enter Principle Amount : ";
+			cin>>p; 
+			cout<<"\n\t   Enter the rate of interest : ";
+			cin>>r;
+			cout<<"\n\t   Enter the time : ";
+			cin>>t;  
+			ci=p*(pow((1+r/100),t));
+			cout<<"\n+++++++++++++++++++++++++++++++++++++++++++";
+			cout<<"\n\n\t The Compound Interest is : "<<ci<<endl;
+			cout<<"\n+++++++++++++++++++++++++++++++++++++++++++";
+		}
+		else
+			cout<<"\n\t Compound Interest Not Available in Current Account"<<"__________________________________________________";
+		//	getch();
+	}  
+	void info()
+	{
+		system("cls");
+		system("color d");
+		cout<<"\n\n";
+		cout<<"\n\t Name : "<<F_name<<" "<<L_name;
+		cout<<"\n\t Account No : "<<Acc_ID;
+		cout<<"\n\n\t\t\t\t\t\t\t\t Age : "<<age;
+		cout<<"\n\n\t\t\t\t\t\t\t\t Email-Id : "<<email;
+		cout<<"\n\n\t\t\t\t\t\t\t\t Mobile NO : "<<mobi;
+		if(type=='S' || type=='s')
+			cout<<"\n\n\t\t\t\t\t\t\t\t Type of Account : Saving";
+		else
+			cout<<"\n\n\t\t\t\t\t\t\t\t Type of Account : Current";
+		cout<<"\n\n\t\t\t\t\t\t\t\t Available Balance : Rs."<<blnc;
 
+		//getch();
+		system("color 7");
+	}       
+};
 int main()
 {
-    Customer cust_obj;
-    // cust_obj.get_data();
-    cust_obj.get_data_cards();
-    return 0;   
+	int j=0,ch;
+	Transaction obj;
+	do{
+		cout <<"*********Wellcome*******";
+		cout<<"\n\t 1. Costomer Data ";
+		cout<<"\n\t 2. Account Data ";
+		cout<<"\n\t 3. Display ";
+		cout<<"\n\t 4. Deposit Amount ";
+		cout<<"\n\t 5. Withdraw Amount ";
+		cout<<"\n\t 6. Balance Enquiry ";
+		cout<<"\n\t 7. Check Book Facility";
+		cout<<"\n\t 8. Calculate Compound Interest ";
+		cout<<"\n\t 9. Account Info";
+		cout<<"\n\t  Exit(0)  \t\t Sign Out(s)";
+		cout<<"\n+++++++++++++++++++++++++++++++++++++++++++";
+		cout<<"\n\t  Enter your choice : ";
+		cin>>ch;
+
+		if(ch==1){
+			obj.userdata();
+			j=0;
+		}
+		else if(ch==2){
+			obj.UserData();
+			j=0;
+		}
+		else if(ch==3){
+			obj.displ();
+			j=0;
+		}
+		else if(ch==4){
+			obj.deposit();
+			j=0;
+		}
+		else if(ch==5){
+			obj.withdraw();
+			j=0;
+		}
+		else if(ch==6){
+			obj.blncenquiry();
+			j=0;
+		}
+		else if(ch==7){
+			obj.checkbk();
+			j=0;
+		}
+		else if(ch==8){		
+			obj.compound();
+			j=0;
+		}
+		else if(ch==9){
+			obj.info();
+			j=0;
+		}
+		else if(ch==0){
+			j=1;
+		}
+		else{
+			j=0;
+		}
+
+	}while(j==0);
+	obj.nam();
+
+
+	return 0;
 }
